b0ad08a3e9687da531fa6b11e6946b73d67cc655
ALLUKISHORE <98156472+ALLUKISHORE@users.noreply.github.com>
2022-04-07 04:58:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,12 +11,12 @@ from config import CHANNEL, GROUP, OWNER
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "Aʙᴏᴜᴛ":
+    if data == "about":
         await query.message.edit_text(
-            text=f"<b>Aʙᴏᴜᴛ:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n<b>",
+            text=f"<b>about:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n<b>",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
+                [[InlineKeyboardButton("• close •", callback_data="close")]]
             ),
         )
     elif data == "close":
